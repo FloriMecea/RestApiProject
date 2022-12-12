0b0df27f2208f11f1b874157e24f8a7613758211
Florentina Manescu <florentina.manescu@softelligence.net>
2022-12-12 11:12:43 +0200

    curs2 restAPI

---
+++ src/main/java/tests/RestExampleTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.Test;
+//import io.restassured.RestAssured;
+
+import io.restassured.response.Response;
+
+public class RestExampleTest {
+
+	@Test
+	public void postATodo() {
+		// URL-ul unde vom face callul: https://keytodorestapi.herokuapp.com/
+		
+		Response response;
+	}
+	
+	
+}
