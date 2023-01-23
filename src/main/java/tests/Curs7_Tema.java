caeeb82397adfa9f4488b9799b6c4c424e4cde24
Florentina Manescu <florentina.manescu@softelligence.net>
2023-01-23 21:44:29 +0200

    tema curs7

---
+++ src/main/java/tests/Curs7_Tema.java
@@ -0,0 +1,62 @@
+package tests;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static utils.NumberChecker.*;
+import static utils.NumberIsPositive.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+public class Curs7_Tema {
+	
+	//https://swapi.dev/api/people/1/
+
+	@Test
+	public void test() {
+		Response resp=given().
+				get("https://swapi.dev/api/people/1/").
+				then().
+				statusCode(200).extract().response();
+		
+		System.out.println(resp.asString());
+		JsonPath jsonPath=resp.jsonPath();
+		
+		assertThat(jsonPath.getString("name"), equalTo("Luke Skywalker"));
+		
+		
+		assertThat(jsonPath.getInt("height"), greaterThan(171));
+		assertThat(jsonPath.getInt("mass"), lessThan(80));
+		
+       List<String> details= new ArrayList<>(); 
+       details.add(jsonPath.getString("skin_color"));
+       details.add(jsonPath.getString("eye_color"));
+       details.add(jsonPath.getString("hair_color"));
+		
+	   assertThat(details,contains(equalTo("fair"),equalTo("blue"),equalTo("blond")));
+	   
+	   assertThat(jsonPath.getString("birth_year"),is(not(numberOnly())));
+		
+	   List<String> species= jsonPath.getList("species");
+	   assertThat(species, is(empty()));
+	   
+	   List<String> vehicles= jsonPath.getList("vehicles");   
+	   System.out.println(vehicles);
+	   List<String> starships= jsonPath.getList("starships");
+	   System.out.println(starships);
+	   assertThat(starships, hasSize(starships.size()));
+	   
+	   for (String element:vehicles) {
+		   assertThat(element,not(containsString("starship")));
+	   }
+	   
+	}
+ 
+	  
+	
+}
